6764c5012ee94aa81fb2fa0436f8aee34b33e15b
mbernal-git <mbernal.sandbox@outlook.com>
2024-09-03 06:03:15 +0800

    added manual on/off; get pump status; monitor status

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
 # TODO: Import your module here
-from modules import WaterLevelModule
+# from modules import WaterLevelModule
+from modules import PumpModule
+from time import sleep
 
 class WaterFeeder:
-    def __init__(self, waste_water_level_sensor):
+    def __init__(self, pumpArg):
         # Set your module as member here
-        self.waste_water_level_sensor = waste_water_level_sensor
+        # self.waste_water_level_sensor = waste_water_level_sensor
+        self.pump = pumpArg
         # GET WATER FEEDER INITIAL DATA
         self.get_sensor_data()
 
     def get_sensor_data(self):
-        waste_water_level = self.waste_water_level_sensor.get_water_level()
-        print(waste_water_level)
+        # waste_water_level = self.waste_water_level_sensor.get_water_level()
+        # print(waste_water_level)
+        # print(pump.get_status())
+        pass
 
     def monitor_waste_water_level(self):
         self.waste_water_level_sensor.monitor_water_level()
 
     def cleanup(self):
-        self.waste_water_level_sensor.cleanup()
+        # self.waste_water_level_sensor.cleanup()
+        self.pump.cleanup()
 
 if __name__ == "__main__":
-    waste_water_level_sensor = WaterLevelModule(in_pin=17, mode_pin=27, sensor_location="waste")
+    # waste_water_level_sensor = WaterLevelModule(in_pin=17, mode_pin=27, sensor_location="waste")
+    pump = PumpModule(pin=14)
 
     try:
         # Instantiate your module here by passing them as args
         water_feeder = WaterFeeder(
-            waste_water_level_sensor = waste_water_level_sensor
+            # waste_water_level_sensor = waste_water_level_sensor,
+            pumpArg = pump
         )
         # Start monitoring here
-        water_feeder.monitor_waste_water_level()
+        # water_feeder.monitor_waste_water_level()
+        pump.start()
+        # pump.monitor_status()
+        sleep(3)
+        pump.stop()
 
     except KeyboardInterrupt:
         print("Exiting program...")
--- modules/PumpModule.py
+++ modules/PumpModule.py
@@ -1,30 +1,51 @@
 """
 File: PumpModule
-Author: 
+Author: Mike Bernal
 Description: Turn on/off water pump based on the following conditions:
-1. If bowl valve is open, turn on.
-2. If bowl valve is closed, turn off.
-3. If water waste tank is full, turn off
+1. TODO If bowl valve is open, turn on.
+2. TODO If bowl valve is closed, turn off.
+3. TODO If water waste tank is full, turn off
+4. DONE Manually on/off pump
+5. DONE Get pump status
+6. DONE Continously monitor pump status at certain interval
 """
 import RPi.GPIO as GPIO
-
+from time import sleep
 class PumpModule:
   def __init__(self, pin):
     self.pin = pin
+    self.pins_set = False
+    # Set GPIO interface
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(self.pin, GPIO.IN)
+
+    GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.LOW)
+    self.pins_set = True
+
+    # Pump initially turned off
+    GPIO.output(self.pin, GPIO.LOW)
 
   def start(self):
-    # Work here
-    pass
+    GPIO.output(self.pin, GPIO.HIGH)
+    
 
   def stop(self):
-    # Work here
-    pass
+    GPIO.output(self.pin, GPIO.LOW)
 
   def get_status(self):
-    # Work here
-    pass
+    return GPIO.input(self.pin)
+  
+  def monitor_status(self):
+    try:
+      while True:
+        pump_status = self.get_status()
+        print(f"Pump status: {'ON' if pump_status else 'OFF'}")
+        sleep(5)
+    except KeyboardInterrupt:
+      print("Force stopping pump")
+    finally:
+      self.cleanup()
 
   def cleanup(self):
-    GPIO.cleanup()
+    if self.pins_set:
+      GPIO.cleanup(self.pin)
+      self.pins_set = False
